655b513610f8fd1cdc385b88554d903c299253f3
YD-YC <146610795+YD-YC@users.noreply.github.com>
2023-10-07 19:35:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,31 @@ document.querySelector("h1").onmouseover = event => {
         iteration += 1 / 3;
     }, 30);
 }
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown =>{
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelectorAll('.menu li');
+
+
+    select.addEventListener('click',() => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(options =>{
+        option.addEventListener('click',() =>{
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option =>{
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
