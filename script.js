7f05fc8a640e5c27496c1e44d2aa9fba6553f170
Hemant <139769730+Hemantgithubpro@users.noreply.github.com>
2023-10-08 05:03:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
